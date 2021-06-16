110523c5bd9da1fa3679bc875c63387593a91f9c
Rafael Botazini <rafaelbotazini@gmail.com>
2021-06-16 19:00:58 -0300

    Some awesome tweaks

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -160,7 +160,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "", "", "", "", "", "ﱘ", "", "", "" }, s, awful.layout.layouts[1])
+    awful.tag({ "", "", "", "", "", "ﱘ", "", "", "" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
